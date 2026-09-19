12946dd3330991726d46eaf496c67d0d62ca7cfa
astridt3 <rocio.beck05@gmail.com>
2026-09-19 18:43:04 -0300

    movimiento

--- Assets/Scripts/Level Shit/HostOnlyLevelMenu.cs
+++ Assets/Scripts/Level Shit/HostOnlyLevelMenu.cs
@@ -1,18 +1,18 @@
-using Fusion;
-using UnityEngine;
-using UnityEngine.UI;
+//using Fusion;
+//using UnityEngine;
+//using UnityEngine.UI;
 
-public class HostOnlyLevelMenu : NetworkBehaviour
-{
-    [SerializeField] private Button[] levelButtons;
+//public class HostOnlyLevelMenu : NetworkBehaviour
+//{
+//    [SerializeField] private Button[] levelButtons;
 
-    public override void Spawned()
-    {
-        bool isHost = Runner.IsSceneAuthority;
+//    public override void Spawned()
+//    {
+//        bool isHost = Runner.IsSceneAuthority;
 
-        foreach (Button button in levelButtons)
-        {
-            button.interactable = isHost;
-        }
-    }
-}
+//        foreach (Button button in levelButtons)
+//        {
+//            button.interactable = isHost;
+//        }
+//    }
+//}
